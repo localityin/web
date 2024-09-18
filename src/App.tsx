69ba6aab703a58fa29eda0b63819856a93d8638e
porcelaincode <allstarworkburner@gmail.com>
2024-09-19 00:24:12 +0530

    useLocation app crash fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,29 @@ import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
-// components
 import Header from './components/Header';
-
-// router
 import router from './router';
 
-function App() {
+const TrackPageView = () => {
   const location = useLocation();
 
   useEffect(() => {
     const trackingId = process.env.REACT_APP_GA_MEASUREMENT_ID;
-
     if (trackingId) {
       ReactGA.initialize(trackingId);
       ReactGA.send({ hitType: 'pageview', page: location.pathname });
     }
   }, [location]);
 
+  return null;
+};
+
+const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      {/* Only here to track page views */}
+      <TrackPageView />
       <Routes>
         <Route path="/">
           {router.map((value) =>
@@ -36,6 +38,6 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
